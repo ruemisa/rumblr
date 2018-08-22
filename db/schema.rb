37d6b689fcabea41b83f1d1486ad4fe6816da11e
Ruel Misa <ruel.g.misa@gmail.com>
2018-08-22 12:48:03 -0400

    Global posts

--- db/schema.rb
+++ db/schema.rb
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_152655) do
     t.string "post_category"
     t.string "author"
     t.string "theme"
+    t.string "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "user_id"
